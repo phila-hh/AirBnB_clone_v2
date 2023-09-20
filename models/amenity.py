2f1721a9f4932ecff8c977cb139595a793de23b0
AropetJ <aropetjoel32@gmail.com>
2023-09-20 21:52:25 +0300

    Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class """
+
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
